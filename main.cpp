8af1e66d0f24fa8d4c509cf48ffabb15cb9e1baa
ProstoNekitos <nolvenon@gmail.com>
2020-02-20 18:48:26 +0300

    Getting started

--- main.cpp
+++ main.cpp
@@ -2,25 +2,42 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main() {
     if( !glfwInit() ){
         std::cout << "Some bad shit happened. glfw was not initialized \n";
         return 1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    GLFWwindow* window = glfwCreateWindow(100,100, "Blank", nullptr, nullptr);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800,600, "Blank", nullptr, nullptr);
 
     if( !window ){
+        glfwTerminate();
         std::cout << "Some bad shit happened. Window was not created \n";
         return 1;
     }
-
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glViewport(0, 0, 800, 600);
 
     while (!glfwWindowShouldClose(window))
     {
-        // Keep running
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
 
