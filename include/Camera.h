405cd184f1b8ce98a03e3565be2ad1903fa24a5f
ProstoNekitos <nolvenon@gmail.com>
2020-03-12 16:42:02 +0300

    [not buildable]; Camera rework #2

--- include/Camera.h
+++ include/Camera.h
@@ -23,33 +23,17 @@ public:
         DOWN
     };
 
-    // Camera Attributes
-    glm::vec3 Position{};
-    glm::vec3 Front;
-    glm::vec3 Up{};
-    glm::vec3 Right{};
-    glm::vec3 WorldUp{};
-
-    // Euler Angles
-    double Yaw;
-    double Pitch;
-
-    // Camera options
-    float MovementSpeed;
-    float MouseSensitivity;
-    float Zoom;
-
     // Constructor with vectors
-    explicit Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH)
+    explicit Camera(glm::vec3 position = glm::vec3(0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH)
     : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
     MovementSpeed(SPEED),
     MouseSensitivity(SENSITIVITY),
-    Zoom(ZOOM)
+    Zoom(ZOOM),
+    Position(position),
+    WorldUp(up),
+    Yaw(yaw),
+    Pitch(pitch)
     {
-        Position = position;
-        WorldUp = up;
-        Yaw = yaw;
-        Pitch = pitch;
         updateCameraVectors();
     }
 
@@ -114,6 +98,22 @@ public:
         if (Zoom >= 45.0f)
             Zoom = 45.0f;
     }
+
+    // Camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right{};
+    glm::vec3 WorldUp{};
+
+    // Euler Angles
+    double Yaw;
+    double Pitch;
+
+    // Camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
 
 private:
     void updateCameraVectors()
